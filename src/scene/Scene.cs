78c068034436ff738a26f8e06be41f917bf04614
chinpteo <chinphin.teo@gmail.com>
2021-09-04 23:43:07 +1000

    Working on diffuse

--- src/scene/Scene.cs
+++ src/scene/Scene.cs
@@ -120,7 +120,10 @@ namespace RayTracer
                         Vector3 new_p = hit.Normal + hit.Position + random_in_unit_sphere();
                         Ray new_ray = new Ray(hit.Position, (new_p - hit.Position).Normalized());
 
-                        pixelColor = entity.Material.Color * (new Color(0.5, 0.5, 0.5) * Colorizer(new_ray, bounce - 1));
+                        PointLight alight = this.lights.First();
+                        Color a_color = alight.Color * entity.Material.Color * hit.Normal.Dot(alight.Position - hit.Position)   ;
+                        // pixelColor = entity.Material.Color * (new Color(0.5, 0.5, 0.5) * Colorizer(new_ray, bounce - 1));
+                        pixelColor = a_color;
 
                     }
                 }
